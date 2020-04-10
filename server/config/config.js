fd47b4ae8c806f897827a34d67fd4bee7cb689fb
Isai <isai.reveles@icloud.com>
2020-04-09 20:47:20 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 //  PUERTO
 // =================
 process.env.PORT = process.env.PORT || 3000;
+
+// =================
+//  ENTORNO
+// =================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =================
+//  BASE DE DATOS
+// =================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jisair:g8bQVAeg6Db8DHl4@cluster0-4bh7a.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require('./config/config');
 const express = require('express');
 const app = express();
-
+// Using Node.js `require()`
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -9,35 +10,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('GET usuario')
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
 });
-
-
-//crear registros
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({ persona: body });
-    }
-});
-
-//actualizar
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('DELETE usuario')
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:')); // enlaza el track de error a la consola (proceso actual)
+db.once('open', () => {
+    console.log('Base de datos ONLINE'); // si esta todo ok, imprime esto
 });
 
 app.listen(process.env.PORT, () => {
